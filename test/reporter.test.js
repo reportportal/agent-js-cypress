3771f0e92259169f790f1ba4640b0875f0020510
Thomas Winkler <twi@softwareag.com>
2023-01-19 12:34:00 +0100

    Minor improvements in reporter.test.js. Added testcase for correct usage of videosFolder.

--- test/reporter.test.js
+++ test/reporter.test.js
@@ -181,7 +181,7 @@ describe('reporter script', () => {
 
     beforeAll(() => {
       mockFS({
-        'example/screenshots/example.spec.js': {
+        example: {
           videos: {
             'custom suite name.cy.ts.mp4': Buffer.from([1, 2, 7, 9, 3, 0, 5]),
           },
@@ -191,6 +191,7 @@ describe('reporter script', () => {
 
     afterAll(() => {
       mockFS.restore();
+      reporter.config.reporterOptions.videosFolder = undefined;
     });
 
     this.beforeEach(() => {
@@ -204,6 +205,7 @@ describe('reporter script', () => {
         { id: 'suite', title: 'any suite' },
       ];
       reporter.testItemIds.set('root', 'suiteTempId');
+      reporter.config.reporterOptions.videosFolder = 'example/videos';
     });
 
     afterEach(() => {
@@ -291,6 +293,21 @@ describe('reporter script', () => {
         status: 'failed',
       };
 
+      reporter.suiteEnd(suiteEndObject);
+
+      expect(spySendVideoOnFinishSuite).not.toHaveBeenCalled();
+    });
+
+    it('sendLog with video attachment - do not send if video not found in videosFolder', function() {
+      const spySendVideoOnFinishSuite = jest.spyOn(reporter.client, 'sendLog');
+
+      const suiteEndObject = {
+        id: 'root',
+        title: 'suite title',
+        status: 'failed',
+      };
+
+      reporter.config.reporterOptions.videosFolder = 'example/screenshots';
       reporter.suiteEnd(suiteEndObject);
 
       expect(spySendVideoOnFinishSuite).not.toHaveBeenCalled();
