262695181c56ea0cb76359ea0dc8a04498c3cbe7
Ilya Hancharyk <Ilya_Hancharyk@epam.com>
2024-09-23 16:09:03 +0200

    EPMRPP-94905 || Time with microseconds support

--- test/mock/mocks.js
+++ test/mock/mocks.js
@@ -1,4 +1,20 @@
-const currentDate = new Date().valueOf();
+/*
+ *  Copyright 2024 EPAM Systems
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+const currentDate = '2024-09-23T12:20:59.392987Z';
 
 class RPClient {
   constructor(config) {
@@ -27,10 +43,6 @@ class RPClient {
     this.sendLog = jest.fn().mockReturnValue({
       promise: Promise.resolve('ok'),
     });
-
-    this.helpers = {
-      now: jest.fn().mockReturnValue(currentDate),
-    };
   }
 }
 
